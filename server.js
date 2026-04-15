3cddb13db683981b283c3aec1d8712be87f0b3cd
Nihar <niharkanchamreddy@gmail.com>
2026-04-15 15:10:37 +0530

    fix railway port

--- server.js
+++ server.js
@@ -3,11 +3,18 @@ import fs from "fs";
 import cors from "cors";
 const app = express();
 app.use(cors());
+
 app.get("/jobs", (req, res) => {
   const jobs = JSON.parse(fs.readFileSync("top_jobs.json"));
   res.json(jobs);
+}); 
+
+app.get("/", (req, res) => {
+  res.send("AI Job Hunter Backend Running 🚀");
 });
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () => console.log(`Server running on ${PORT}`));
+app.listen(PORT, () => {
+  console.log(`Server running on ${PORT}`);
+});
